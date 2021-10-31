9f447af220e93af6314a45127131ab8fce517e5f
youngsikwon <youngsik3200@gmail.com>
2021-10-31 22:55:52 +0900

    first

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 const gulp = require('gulp');
+const bable = require('gulp-babel');
+
+
 
 gulp.task('default', function () {
-    
+ //노드 소스
+ gulp.src("es6/**/*.js")
+ .pipe(bable())
+ .pipe(gulp.dest("dist"));
+ //브라우저 소스
+ gulp.src("es6/**/*.js")
+ .pipe(bable())
+ .pipe(gulp.dest("public/dist"));    
 });
---
+++ public/es6/test.js
@@ -0,0 +1,6 @@
+'use strict';
+//es6 기능 : 블록 스코프 변수 선언
+const sentenes = [
+    {subject : 'Javscript', verb: 'is', object : 'great'},
+    {subject : 'Elphants', verb: 'are', object : 'large'},
+]
